99489b7746035c287a2f07317aaa4dad2d18cee0
ZohaibQaiserAhmad <30640101+ZohaibQaiserAhmad@users.noreply.github.com>
2021-04-09 22:29:54 -0400

    Finished backend routing
    Delete List,
    Update List,
    Search List

---
+++ kagamus-api/routes/deleteList.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+var rq = require('request');
+
+var MongoClient = require('mongodb').MongoClient;
+var urls = "mongodb://localhost:27017/";
+
+
+router.post('/', function (req, res, next) {
+
+  var jsonString = req.body;
+  console.log(jsonString);
+  var jsonRequest = JSON.parse(JSON.stringify(jsonString));
+  var query = {Genre : jsonRequest.Genre, username : jsonRequest.username, AnimeListTitle : jsonRequest.AnimeListTitle, AnimeList : jsonRequest.AnimeList}
+
+  MongoClient.connect(urls, { useUnifiedTopology: true }, function (err, db) {
+    if (err) throw err;
+    var dbo = db.db("AnimeList");
+
+    dbo.collection("myLists").deleteOne(query);
+    res.send(query);
+    
+  });
+
+
+});
+
+module.exports = router;
--- kagamus-api/routes/findanimeList.js
+++ kagamus-api/routes/findanimeList.js
@@ -8,31 +8,24 @@ var urls = "mongodb://localhost:27017/";
 
 router.post('/', function (req, res, next) {
 
-
     var jsonString = req.body;
     var jsonRequest = JSON.parse(JSON.stringify(jsonString));
     var query = {Genre : jsonRequest.Genre, username : jsonRequest.username, AnimeListTitle : jsonRequest.AnimeListTitle, AnimeList : jsonRequest.AnimeList}
 
-  
+    MongoClient.connect(urls, { useUnifiedTopology: true }, function (err, db) {
+        if (err) throw err;
+        var dbo = db.db("AnimeList");
 
-  MongoClient.connect(urls, { useUnifiedTopology: true }, function (err, db) {
-    if (err) throw err;
-    var dbo = db.db("AnimeList");
-
-    dbo.collection("myLists").insert(query);
-    res.send(query);
-    
-  });
-});
-
-
-router.post('/deleteList', function (req, res, next) {
-
-  var jsonString = req.body;
-  var jsonRequest = JSON.parse(JSON.stringify(jsonString));
-  console.log(jsonRequest);
-
-  res.send(query);
+        dbo.collection("myLists").find(query).limit(1).next(function(err, doc){
+            // handle data
+            console.log(err);
+            console.log(doc);
+            res.send(objectID(doc._id));
+         })
+       
+     
+        
+    });
 
 
 });
